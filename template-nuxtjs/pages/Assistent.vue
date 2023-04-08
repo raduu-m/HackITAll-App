9bcbcb5c84e29b0408dfc4c841d5f38bff456736
sebastian-nae <naesebi2000@gmail.com>
2023-04-08 18:51:15 +0300

    Added QR and Chat Bot

--- template-nuxtjs/pages/Assistent.vue
+++ template-nuxtjs/pages/Assistent.vue
@@ -1,86 +1,120 @@
 <template>
-    <v-app>
-      <v-card class="elevation-12" style="; margin: 50px auto; height: 700px;  max-width: 100%;
-      width: 800px;
-      margin: 0 auto;">
-        <v-toolbar color="primary" dark>
-          <v-toolbar-title>AI Assistant Chat</v-toolbar-title>
-          <v-spacer></v-spacer>
-        </v-toolbar>
-        <v-card-text style="height: 450px; overflow-y: scroll;">
-          <v-list>
-            <v-list-item v-for="(message, index) in messages" :key="index">
-              <v-list-item-content>
-                <v-card class="pa-2" :class="{ 'blue-grey': message.sender === 'bot', 'green lighten-3': message.sender === 'user' }">
-                  <v-card-title v-if="message.sender === 'bot'" class="text-h6">AI Assistant:</v-card-title>
-                  <v-card-title v-else class="text-h6">You:</v-card-title>
-                  <v-card-text style="width: 100% margin: 50px auto">{{ message.content }}</v-card-text>
-                </v-card>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-card-text>
-        <v-card-actions>
-          <v-form @submit.prevent="sendMessage">
-            <v-textarea
-              v-model="messageContent"
-              label="Type your message here"
-              :rows="1"
-              auto-grow
-              clearable
-              outlined
-              dense
-              required
-            ></v-textarea>
-            <v-btn color="primary" type="submit" :disabled="!messageContent.trim()">Send</v-btn>
-          </v-form>
-        </v-card-actions>
-      </v-card>
-    </v-app>
-  </template>
+  <v-app>
+    <v-card class="elevation-12" style="; margin: 50px auto; height: 700px;  max-width: 100%;
+            width: 800px;
+            margin: 0 auto;">
+      <v-toolbar color="primary" dark>
+        <v-toolbar-title>AI Assistant Chat</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-card-text style="height: 450px; overflow-y: scroll;">
+        <v-list>
+          <v-list-item v-for="(message, index) in messages" :key="index">
+            <v-list-item-content>
+              <v-card class="pa-2"
+                :class="{ 'blue-grey': message.sender === 'bot', 'green lighten-3': message.sender === 'user' }">
+                <v-card-title v-if="message.sender === 'bot'" class="text-h6">AI Assistant:</v-card-title>
+                <v-card-title v-else class="text-h6">You:</v-card-title>
+                <v-card-text style="width: 100% margin: 50px auto">{{ message.content }}</v-card-text>
+              </v-card>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+      <v-card-actions>
+        <v-form @submit.prevent="sendMessage">
+          <v-textarea v-model="messageContent" label="Type your message here" :rows="1" auto-grow clearable outlined dense
+            required></v-textarea>
+          <v-btn color="primary" type="submit" :disabled="!messageContent.trim()">Send</v-btn>
+        </v-form>
+      </v-card-actions>
+    </v-card>
+  </v-app>
+</template>
   
-  <script>
-  import axios from 'axios'
-  
-  export default {
-    name: 'AIChatPage',
-    data: () => ({
-      messages: [],
-      messageContent: ''
-    }),
-    methods: {
-      async sendMessage () {
-        if (!this.messageContent.trim()) {
-          return
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'AIChatPage',
+  data: () => ({
+    messages: [],
+    messageContent: ''
+  }),
+  methods: {
+    async queryData(data) {
+      const response = await fetch(
+        "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill",
+        {
+          headers: { Authorization: "Bearer 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" },
+          method: "POST",
+          body: JSON.stringify(data),
         }
-        this.messages.push({
-          sender: 'user',
-          content: this.messageContent
-        })
-        const response = await this.getResponse(this.messageContent)
-        this.messages.push({
-          sender: 'bot',
-          content: response.data.response
-        })
-        this.messageContent = ''
-      },
-      async getResponse (message) {
-        const postData = {
-          message: message
-        }
-        return axios.post('/api/ai-chat', postData)
-      },
-      logout () {
-        localStorage.removeItem('token')
-        this.$router.push('/login')
+      );
+      const result = await response.json();
+      return result;
+    },
+    async sendMessage() {
+      let past_user_inputs = []
+      let generated_responses = []
+      let responses_bot = null;
+
+      // let output = await this.queryData({
+      //   "inputs": {
+      //     "past_user_inputs": [],
+      //     "generated_responses": ["It's Die Hard for sure."],
+      //     "text": "Can you explain why ?"
+      //   }
+      // }).then((response) => {
+      //   console.log(JSON.stringify(response));
+      // });
+      if (!this.messageContent.trim()) {
+        return
       }
+      this.messages.push({
+        sender: 'user',
+        content: this.messageContent
+      })
+      past_user_inputs.push(this.messageContent)
+      await this.queryData({
+        inputs: {
+          past_user_inputs: past_user_inputs,
+          generated_responses: generated_responses,
+          text: this.messageContent
+        }
+      })
+        .then(response => {
+          responses_bot = response;
+          console.log('Success:', response.conversation.generated_responses[0]);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+      this.messages.push({
+        sender: 'bot',
+        content: responses_bot.conversation.generated_responses[0]
+      })
+      console.log(responses_bot)
+      generated_responses.push(responses_bot.conversation.generated_responses[0])
+      this.messageContent = ''
+    },
+    // async getResponse(message) {
+    //   const postData = {
+    //     message: message
+    //   }
+    //   return axios.post('/api/ai-chat', postData)
+    // },
+    logout() {
+      localStorage.removeItem('token')
+      this.$router.push('/login')
     }
   }
-  </script>
+}
+</script>
   
-  <style scoped>
-  .blue-grey {
-    background-color: #B0BEC5 !important;
-  }
-  </style>
+<style scoped>
+.blue-grey {
+  background-color: #B0BEC5 !important;
+}
+</style>
   
